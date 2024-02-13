efa48f690d67ed541885ebe01cf45883e17a5f52
Modeba <dato1modebadze@gmail.com>
2024-02-13 13:47:20 +0400

    'daily'

---
+++ 5.2 Binary Search - 2D Matrix.py
@@ -0,0 +1,20 @@
+test1 = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+
+def searchMatrix(matrix, target):
+    def search(nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return True
+        return False
+    
+    for i in range(2, len(matrix)):
+        if matrix[i][0] > target:
+            return search(matrix[i - 1], target)
+
+print(searchMatrix(test1, 12))
