a8e91e567f64863b7fa0ba9c1ff0a6595fce2138
Modeba <dato1modebadze@gmail.com>
2024-02-14 10:18:49 +0400

    udpate

--- 5.3 Binary Search - Koko Eating Bananas.py
+++ 5.3 Binary Search - Koko Eating Bananas.py
@@ -1,9 +1,10 @@
 test1 = [3,6,7,11]
 test2 = [30,11,23,4,20]
-test3 = [30,11,23,4,20]
 
 def minEatingSpeed(piles, h):
-    def totalTime(piles, h, k):
+    '''totalTime function goes over every value in Piles and sums up time taken by them.
+    Can be simplified with math.ceiling function'''
+    def totalTime(piles, k):
         totalTime = 0
         for pile in piles:
             if pile % k == 0:
@@ -11,12 +12,19 @@ def minEatingSpeed(piles, h):
             else:
                 hoursPerPile = pile // k + 1
             totalTime += hoursPerPile
-        if totalTime < h:
-            return k
-    
-    for k in range(1, max(piles)):
-        time = totalTime(piles, h, k)
-        if time:
-            return time
+        print(totalTime)
+        return totalTime
+ 
+    '''Binary search between the lowest and the highest possible speeds'''
+    l = 1
+    r = speed = max(piles)
+    while l <= r:
+        m = l + (r - l) // 2
+        if totalTime(piles, m) <= h:
+            speed = min(speed, m)
+            r = m - 1
+        else:
+            l = m + 1
+    return speed
 
-print(minEatingSpeed(test1, 5))
+print(minEatingSpeed(test2, 5))
