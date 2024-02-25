675457d95cbcae7addeddb841f09e59f1dec9949
Modeba <dato1modebadze@gmail.com>
2024-02-26 00:46:39 +0400

    Daily leetcode

---
+++ Leetcode 17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,26 @@
+def letterCombinations(self, digits: str) -> List[str]:
+    if len(digits) == 0:
+        return []
+
+    phone_map = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    }
+
+    def backtrack(combination, next_digits):
+        if len(next_digits) == 0:
+            output.append(combination)
+        else:
+            for letter in phone_map[next_digits[0]]:
+                backtrack(combination + letter, next_digits[1:])
+    
+    output = []
+    backtrack("", digits)
+
+    return output
