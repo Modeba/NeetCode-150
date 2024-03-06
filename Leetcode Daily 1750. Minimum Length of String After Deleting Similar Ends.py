741e87cc878d054ecda92924b603e4c7206a9bee
Modeba <dato1modebadze@gmail.com>
2024-03-06 10:41:58 +0400

    Daily Leetcode

--- Leetcode Daily 1750. Minimum Length of String After Deleting Similar Ends.py
+++ Leetcode Daily 1750. Minimum Length of String After Deleting Similar Ends.py
@@ -3,18 +3,15 @@ test2 = "aa"
 test3 = "a"
 
 def minimumLength(s):
-    while len(s) > 0 and s[0] == s[-1]:
-        print(s)
-        left = 0
-        right = len(s) - 1
-        while left < right:
-            print(left, right)
-            while s[left] == s[0]:
-                left += 1
-            while s[right] == s[0]:
-                right -= 1
-            s = s[left + 1 : right]
+    left, right = 0, len(s) - 1
     
-    return len(s)
+    while left < right and s[left] == s[right]:
+        char = s[left]
+        while left <= right and s[left] == char:
+            left += 1
+        while right >= left and s[right] == char:
+            right -= 1
+    
+    return right - left + 1
     
 print(minimumLength(test1))
