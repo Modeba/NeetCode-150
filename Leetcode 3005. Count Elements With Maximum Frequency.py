3fbbe404ec1cdf11c439b433e8402d9e37e3764c
Modeba <dato1modebadze@gmail.com>
2024-03-08 13:38:08 +0400

    Daily Leetcode

--- Leetcode 3005. Count Elements With Maximum Frequency.py
+++ Leetcode 3005. Count Elements With Maximum Frequency.py
@@ -1,19 +1,20 @@
 test1 = [1,2,2,3,1,4]
 test2 = [1,2,3,4,5]
+test3 = [93]
+test4 = [46,64]
+test5 = [14,50,86,23,59,95,32,68,5,41,77,14,50,86,23,59,95,32,68,5,41,77,14,50,86,23,59,95,32,68,5,41,77,14,50,86,23,59,95,32,68,5,41,77,14,50,86,23,59,95,32,68,5,41,77,14,50,86,23,59,95,32,68,5,41,77,14,50,86,23,59,95,32,68,5,41,77,14,50,86,23,59,95,32,68,5,41,77,14,50,86,23,59,95,32,68,5,41,77]
 
 def maxFrequencyElements(nums):
     Map = {}
-
+    max_freq = 1
     for num in nums:
         if num not in Map:
             Map[num] = 1
         else:
             Map[num] += 1
+            max_freq = max(max_freq, Map[num])
 
-    max_freq = 0
-    for num in Map:
-        max_freq = max(max_freq, Map[num])
-
+    print(Map)
     res = 0
     for num in Map:
         if Map[num] == max_freq:
@@ -21,4 +22,4 @@ def maxFrequencyElements(nums):
     
     return res
 
-print(maxFrequencyElements(test2))
+print(maxFrequencyElements(test5))
