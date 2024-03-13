a0c46b9e72bb061d564d79468c7457c46d255b43
Modeba <dato1modebadze@gmail.com>
2024-03-13 10:26:07 +0400

    Daily Leetcode

--- Leetcode 2485. Find the Pivot Integer.py
+++ Leetcode 2485. Find the Pivot Integer.py
@@ -1,16 +1,28 @@
-def pivotInteger(n):
+import time
 
+def pivotInteger(n):
+    '''
     total = 0
     for i in range(1, n + 1):
         total += i
+    '''
+    
+    if n % 2 == 1:
+        total = (n + 1) * (n // 2) + (n // 2 + 1)
+    else:
+        total = (n + 1) * (n // 2)
     
     cumulative_sum = 0
     for i in range(1, n + 1):
         if (total - i) / 2 == cumulative_sum:
-            return i
+            return [n, i]
         cumulative_sum += i
-        
-    return -1
 
-for i in range(1, 11):
-    print(pivotInteger(i))
+start = time.time()
+
+for i in range(1000, 10001):
+    result = pivotInteger(i)
+    if result:
+        print(result)
+
+print(time.time() - start)
