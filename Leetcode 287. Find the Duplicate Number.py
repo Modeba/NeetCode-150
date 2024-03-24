6a9e85463aaa0ccf9d6f00b3aa43e357ec239503
Modeba <dato1modebadze@gmail.com>
2024-03-24 18:21:04 +0400

    Daily Neetcode

--- Leetcode 287. Find the Duplicate Number.py
+++ Leetcode 287. Find the Duplicate Number.py
@@ -1,8 +1,24 @@
-def findDuplicate(nums):
+def findDuplicate1(nums):
     for i in range(len(nums)):
         while i != nums[i]:
             if nums[i] == nums[nums[i]]:
                 return nums[i]
             nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
 
-print(findDuplicate([1,3,4,2,2]))
+def findDuplicate2(nums):
+    slow, fast = nums[0], nums[0]
+
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+    
+    fast = nums[0]
+    while slow != fast:
+        fast = nums[fast]
+        slow = nums[slow]
+    
+    return slow
+
+print(findDuplicate2([1,3,4,2,2]))
