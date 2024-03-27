d1901b6e764812b9385822c788f8d76e0e5545f3
Modeba <dato1modebadze@gmail.com>
2024-03-27 21:44:29 +0400

    Daily leetcode

---
+++ Leetcode 713. Subarray Product Less Than K.py
@@ -0,0 +1,16 @@
+def numSubarrayProductLessThanK(nums, k):
+    l = res = 0
+    product = 1
+
+    for r in range(len(nums)):
+        product *= nums[r]
+
+        while product >= k and l <= r:
+            product /= nums[l]
+            l += 1
+
+        res += r - l + 1
+    
+    return res
+
+print(numSubarrayProductLessThanK([10,5,2,6], 100))
