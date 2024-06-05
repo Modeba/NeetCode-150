e9b9a577791f884045b9f306fdde58305d658f73
Modeba <dato1modebadze@gmail.com>
2024-06-05 10:47:34 +0400

    Daily Neetcode

--- 1.7 Arrays & Hashing - Valid Sudoku.py
+++ 1.7 Arrays & Hashing - Valid Sudoku.py
@@ -1,7 +1,7 @@
 test1 = [[
   "5","3",".", ".","7",".", ".",".","."]
 ,["6",".",".", "1","9","5", ".",".","."]
-,[".","9","5", ".",".",".", ".","6","."]
+,[".","9",".", ".",".",".", ".","6","."]
 
 ,["8",".",".", ".","6",".", ".",".","3"]
 ,["4",".",".", "8",".","3", ".",".","1"]
@@ -25,24 +25,23 @@ test2 = [[
 ,[".",".",".", ".","8",".", ".","7","9"]]
 
 def isValidSudoku(board):
-    rows  = {0:[], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[]}
-    cols  = {0:[], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[]}
-    boxes = {(0,0): [], (0,1): [], (0,2): [], (1,0): [], (1,1): [], (1,2): [], (2,0): [], (2,1): [], (2,2): []}
+    rows    = {0:set(), 1:set(), 2:set(), 3:set(), 4:set(), 5:set(), 6:set(), 7:set(), 8:set()}
+    columns = {0:set(), 1:set(), 2:set(), 3:set(), 4:set(), 5:set(), 6:set(), 7:set(), 8:set()}
+    squares = {(0,0):set(), (0,1):set(), (0,2):set(), (1,0):set(), (1,1):set(), (1,2):set(), (2,0):set(), (2,1):set(), (2,2):set()}
 
     for r in range(len(board)):
         for c in range(len(board[0])):
-            if board[r][c] != '.':
-                if (board[r][c] in rows[r] or
-                    board[r][c] in cols[c] or
-                    board[r][c] in boxes[r//3, c//3]
-                    ):
-                    
-                    print(board[r][c], boxes[r//3, c//3])
-                    return False
-                
-                rows[r].append(board[r][c])
-                cols[c].append(board[r][c])
-                boxes[r//3, c//3].append(board[r][c])
+            if board[r][c] == '.':
+                continue
+            
+            if (board[r][c] in rows[r] or
+                board[r][c] in columns[c] or
+                board[r][c] in squares[(r//3, c//3)]):
+                return False
+            else:
+                rows[r].add(board[r][c])
+                columns[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
 
     return True
     
