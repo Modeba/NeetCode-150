c1e4330101d69fceb906240e33c9c8578c6d65e3
Modeba <dato1modebadze@gmail.com>
2024-08-28 22:23:50 +0400

    Daily Leetcode

--- 3.5 Sliding Window - Minimum Window Substring - Slow.py
+++ 3.5 Sliding Window - Minimum Window Substring - Slow.py
@@ -1,6 +1,27 @@
-test1s = "ADOBECODEBANC"
-test1t = "ABC"
+from collections import Counter
+
+s = "ADOBECODEBANC"
+t = "ABC"
 
 def minWindow(s,t):
-    pass
-    
+    count = Counter(t)
+    l, overall, res = 0, len(count), ''
+
+    for r in range(len(s)):
+        if s[r] in count:
+            count[s[r]] -= 1
+            if count[s[r]] == 0:
+                overall -= 1
+            
+        while overall == 0:
+            if not res or len(res) > r - l + 1:
+                res = s[l:r + 1]
+            if s[l] in count:
+                count[s[l]] += 1
+                if count[s[l]] > 0:
+                    overall += 1
+            l += 1
+        
+    return res
+
+print(minWindow(s, t))
