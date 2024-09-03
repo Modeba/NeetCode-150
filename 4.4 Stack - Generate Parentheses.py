7625a2397bd20e9d9591a93bbeed48cca451f75f
Modeba <dato1modebadze@gmail.com>
2024-09-03 13:18:39 +0400

    Daily Leetcode

--- 4.4 Stack - Generate Parentheses.py
+++ 4.4 Stack - Generate Parentheses.py
@@ -11,13 +11,13 @@ def generateParenthesis(n):
             stack.append('(')
             backtrack(openN + 1, closedN)
             stack.pop()
-            
+        
         if closedN < openN:
             stack.append(')')
             backtrack(openN, closedN + 1)
             stack.pop()
-        
+
     backtrack(0, 0)
     return res
 
-print(generateParenthesis(3))
+print(generateParenthesis(10))
